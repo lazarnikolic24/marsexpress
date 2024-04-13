18d5f989d9ad92d840927f7ed873763dcd1d446a
lazarnikolic24 <lazarnikolic2410@gmail.com>
2024-04-13 19:09:34 +0200

    Dodaj low-res plot

--- source.py
+++ source.py
@@ -1,2 +1,18 @@
-print("Hello World")
+import numpy as np
+import matplotlib.pyplot as plt
+
+t,x,y = np.loadtxt('marsexpresslr.d', usecols = (0,1,2), unpack = True)
+n = len(t)
+dt = t[1] - t[0]
+r = np.zeros((n, 2), float)
+r[:, 0] = x
+r[:, 1] = y
+AU = 129598000.0
+r = r/AU
+
+plt.plot(r[:, 0], r[:, 1])
+plt.axis('equal')
+plt.xlabel('x [au]')
+plt.ylabel('y [au]')
+plt.show()
 
